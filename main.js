155a73b47462c18f7ec6d364499189a8a241a44e
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-19 16:20:28 +0100

    Add particle system to handle dust effects in running state

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ window.addEventListener('load',function(){
             this.input = new InputHandler(this);
             this.UI = new UI(this);
             this.enemies = [];
+            this.particles = [];
             this.enemyTimer = 0;
             this.enemyInterval = 1000;
             this.debug = true;
@@ -43,7 +44,11 @@ window.addEventListener('load',function(){
                 enemy.update(deltaTime);
                 if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
             });
-                
+            //handle particles
+            this.particles.forEach((particle, index) => {
+                particle.update();
+                if(particle.markedForDeletion) this.particles.splice(index, 1);
+            });    
             
         }
         draw(context){
@@ -52,6 +57,9 @@ window.addEventListener('load',function(){
             this.enemies.forEach(enemy =>{
                 enemy.draw(context);
             });
+            this.particles.forEach(particle =>{
+                particle.draw(context);
+            });
             this.UI.draw(context);
         }
         addEnemy(){
@@ -59,13 +67,12 @@ window.addEventListener('load',function(){
             if(this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
             else if(this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
             this.enemies.push(new FlyingEnemy(this));
-            console.log(this.enemies);
+            
         }
 
         
     }
     const game = new Game(canvas.width,canvas.height);
-    console.log(game);
     
     let lastTime = 0;
 
