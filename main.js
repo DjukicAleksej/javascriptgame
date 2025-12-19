e92f89550b62a58ebfd9d21a0bb328eef080b19d
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-19 22:59:38 +0100

    Added the creepster font from Google , which I will use to display the score and time , also implemented lives where there are dog heads symbolizing lives of the player and when there is a collision I take one of the lives , when lives are <= 0 the game is over as I set gameOver = truee

--- main.js
+++ main.js
@@ -33,6 +33,7 @@ window.addEventListener('load',function(){
             this.time = 0;
             this.maxTime = 6000;
             this.gameOver = false;
+            this.lives = 3;
             this.player.currentState = this.player.states[0];
             this.player.currentState.enter();
         }
